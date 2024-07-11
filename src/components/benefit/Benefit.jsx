814cd10925171e58d52f90e9fdeb615f4233f0bc
Zayan-dev <fa21bscs0095@maju.edu.pk>
2024-07-11 19:32:57 +0500

    Baron font add

--- src/components/benefit/Benefit.jsx
+++ src/components/benefit/Benefit.jsx
@@ -5,7 +5,7 @@ const Benefit = () => {
     return (
         <div className='benefit'>
             <div className='benefit-heading'>
-                <p className='mainHeading'>Transforming brands through captivating storytelling</p>
+                <p className='mainHeading'>Transforming brands through captivating storytelling.</p>
             </div>
             <div className='benefit-intro'>
                 <p className='para'>Welcome to GlyphicX, where creativity meets strategy to deliver exceptional brand experiences. Our team of experts is passionate about crafting compelling narratives that resonate with your audience and drive results.</p>

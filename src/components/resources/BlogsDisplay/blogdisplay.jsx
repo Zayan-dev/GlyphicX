24bef857ed808d68c30707fc19cd5e295a21494e
Fatima Zehra <fz8585949@gmail.com>
2024-07-16 12:57:58 +0500

    Blog display animation comments

--- src/components/resources/BlogsDisplay/blogdisplay.jsx
+++ src/components/resources/BlogsDisplay/blogdisplay.jsx
@@ -1,9 +1,23 @@
 // components/BlogDisplay.js
-import React from "react";
+import React, {useState, useEffect} from "react";
 import BlogCard from "./../BlogCard/blogCard";
 import "./blogdisplay.css";
 
 const BlogDisplay = ({bgType}) => {
+  // const [currentIndex, setCurrentIndex] = useState(0);
+
+  // const handleSlideTransition = () => {
+  //   setCurrentIndex((currentIndex + 1) % blogs.length);
+  // };
+
+  // useEffect(() => {
+  //   const sliderInterval = setInterval(() => {
+  //     handleSlideTransition();
+  //   }, 5000);
+
+  //   return () => clearInterval(sliderInterval);
+  // }, [currentIndex]);
+
   const blogs = [
     {
       id: 1,
@@ -49,23 +63,27 @@ const BlogDisplay = ({bgType}) => {
     },
     // {
     //   id: 4,
-    //   image: "/images/blog4.jpg",
+    //   image:
+    //     "https://www.simplilearn.com/ice9/free_resources_article_thumb/Is_Graphic_Design_A_Good_Career.jpg",
     //   category: "Travel",
     //   name: "Top 10 Destinations",
     //   description: "The best places to visit this summer.",
     //   authorName: "Emily White",
-    //   authorImage: "/assets/images/glyphicX.png",
+    //   authorImage:
+    //     "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.2113030492.1720224000&semt=ais_user",
     //   date: "June 20, 2024",
     //   readTime: "7 min",
     // },
     // {
     //   id: 5,
-    //   image: "/images/blog5.jpg",
+    //   image:
+    //     "https://www.simplilearn.com/ice9/free_resources_article_thumb/Is_Graphic_Design_A_Good_Career.jpg",
     //   category: "Education",
     //   name: "Online Learning Platforms",
     //   description: "The best platforms for online education.",
     //   authorName: "Michael Green",
-    //   authorImage: "/assets/images/glyphicX.png",
+    //   authorImage:
+    //     "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.2113030492.1720224000&semt=ais_user",
     //   date: "June 15, 2024",
     //   readTime: "5 min",
     // },
@@ -78,6 +96,14 @@ const BlogDisplay = ({bgType}) => {
           <BlogCard key={blog.id} {...blog} bgColor={bgType} />
         ))}
       </div>
+
+      {/* <div className="blogsContainer">
+        {blogs.slice(currentIndex, currentIndex + 3).map((blog) => (
+          <div className="blogCard">
+            <BlogCard key={blog.id} {...blog} bgColor={bgType} />
+          </div>
+        ))}
+      </div> */}
     </>
   );
 };

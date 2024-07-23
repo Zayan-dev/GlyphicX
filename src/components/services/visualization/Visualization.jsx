5c82dc32e87a4c458ae87be5efc280e9a18eb156
Zayan-dev <fa21bscs0095@maju.edu.pk>
2024-07-23 14:28:19 +0500

    3d-visualization slider added

--- src/components/services/visualization/Visualization.jsx
+++ src/components/services/visualization/Visualization.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-import "../visualization/visualization.css"
-import star from "../../../assets/images/Star.png"
+import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import "../visualization/visualization.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import star from "../../../assets/images/Star.png";
+import image1 from "../../../assets/images/slider/3d_1.jpg";
+import image2 from "../../../assets/images/slider/3d_2.jpg";
+import image3 from "../../../assets/images/slider/3d_3.jpg";
 
 const Visualization = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const slides = [
+        { type: 'image', src: image1 },
+        { type: 'image', src: image2 },
+        { type: 'image', src: image3 } 
+    ];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [slides.length]);
+
     return (
         <div className='visualization'>
             <div className="vis-pic">
+            {slides.map((slide, index) => (
+                <div
+                    key={index}
+                    className={`vis-slide ${index === currentIndex ? 'active' : ''}`}
+                >
+                    <img src={slide.src} alt={`slide-${index}`} />
+                </div>
+            ))}
             </div>
 
             <div className='vis-text'>
@@ -19,9 +48,8 @@ const Visualization = () => {
                     <li><img src={star} alt="star" style={{ height: "25px", mixBlendMode: "difference" }} /> Competitive Advantage</li>
                 </div>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default Visualization
+export default Visualization;

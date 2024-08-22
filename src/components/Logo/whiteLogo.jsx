feee7f9a79689d2afab7d97bdcaa75c330fc750b
Zayan-dev <fa21bscs0095@maju.edu.pk>
2024-08-22 17:24:06 +0500

    navbar redesign

---
+++ src/components/Logo/whiteLogo.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import logoImg from './../../assets/images/white.png'
+
+const WhiteLogo = () => {
+  // update the size of the logo when the size of the screen changes
+  const [width, setWidth] = useState(0);
+
+  const updateWidth = () => {
+    const newWidth = window.innerWidth;
+    setWidth(newWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    updateWidth();
+  }, []);
+
+  // change between the logo and the button when the user scrolls
+  const [showButton, setShowButton] = useState(false);
+
+  const changeNavButton = () => {
+    if (window.scrollY >= 400 && window.innerWidth < 768) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavButton);
+  }, []);
+
+  return (
+    <>
+      <Link to="/" style={{ display: "block" }}>
+        {/* <Image
+          src="/assets/images/glyphicX.png"
+          alt="Logo"
+          width={width < 641 ? "35" : "50"}
+          height={width < 641 ? "35" : "50"}
+          className="relative"
+        /> */}
+        <img
+          src={logoImg}
+          alt="Logo"
+          width={width < 641 ? "35" : "70"}
+          height={width < 641 ? "35" : "50"}
+          className="relative"
+        />
+      </Link>
+    </>
+  );
+};
+
+export default WhiteLogo;
--- src/components/Navigations/Navbar/index.jsx
+++ src/components/Navigations/Navbar/index.jsx
@@ -1,87 +1,120 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import Logo from "../../Logo";
+import React, { useState, useEffect } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import Logo from "../../Logo"; // Original bluish logo
 import { CustomButton2 } from "../../CustomButton";
-import Footer from "../../footer/Footer";
+import WhiteLogo from "../../Logo/whiteLogo"; // White logo
 
 const Navbar = ({ toggle }) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const location = useLocation(); // Get the current location
+  const navigate = useNavigate(); // Initialize the navigate function
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    // Only add the scroll event listener if on the homepage
+    if (location.pathname === "/") {
+      window.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [location.pathname]);
+
+  const handleNavigation = (path) => {
+    navigate(path); // Use navigate to change the route
+  };
+
+  // Define styles based on location
+  const isHomePage = location.pathname === "/";
+  const navbarStyles = isHomePage 
+    ? `fixed top-0 z-10 border-b-2 transition-colors duration-300 ${isScrolled ? 'bg-offWhite text-navyBlue' : 'bg-transparent text-white'}`
+    : 'fixed top-0 z-10 bg-white text-navyBlue border-b-2'; // Default styles for other pages
 
   const listItemStyles =
-    "subHeading relative text-lg lg:text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-navyBlue after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center";
+    "subHeading relative text-lg lg:text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300";
     
-  const activeLinkStyles = " after:w-full after:scale-x-0 after:scale-x-100"; 
+  const activeLinkStyles = "after:w-full after:scale-x-0 after:scale-x-100";
 
   return (
-    <>
-      <div id="gsapNavDiv" className="w-full h-16 lg:h-[74px] min-h-[68px] bg-offWhite fixed top-0 z-10 border-navyBlue border-b-2">
-        <div className="w-full px-[6%] md:px-10 lg:px-12 xl:px-16 xl3:px-20 h-full">
-          <div className="h-full relative flex justify-between items-center ">
-            <ul className="hidden md2:flex justify-between gap-6 lg1:gap-12 xl3:gap-20 text-navyBlue">
-              <li>
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    `${listItemStyles} ${isActive ? activeLinkStyles : ""}`
-                  }
-                  id="gsapNavItem"
-                >
-                  <p>Home</p>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/portfolio"
-                  className={({ isActive }) =>
-                    `${listItemStyles} ${isActive ? activeLinkStyles : ""}`
-                  }
-                  id="gsapNavItem"
-                >
-                  <p>Our Work</p>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/services"
-                  className={({ isActive }) =>
-                    `${listItemStyles} ${isActive ? activeLinkStyles : ""}`
-                  }
-                  id="gsapNavItem"
-                >
-                  <p>Services</p>
-                </NavLink>
-              </li>
-            </ul>
-            <div id="gsapLogo" className="static md2:absolute md2:left-1/2 md2:transform md2:-translate-x-1/2">
-              <Logo />
-            </div>
-
-            <NavLink to="#footerSection" id="gsapContactBtn" className="hidden md2:flex xl:w-[180px]">
-              <CustomButton2 text="Contact" />
-            </NavLink>
-
-            <button
-              type="button"
-              className="inline-flex items-center md2:hidden"
-              onClick={toggle}
-            >
-              {/* Menu icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 24 24"
-                color="var(--foreground-color)"
+    <div id="gsapNavDiv" className={`${navbarStyles} w-full h-16 lg:h-[74px] min-h-[68px]`}>
+      <div className="w-full px-[6%] md:px-10 lg:px-12 xl:px-16 xl3:px-20 h-full">
+        <div className="h-full relative flex justify-between items-center">
+          <ul className="hidden md2:flex justify-between gap-6 lg1:gap-12 xl3:gap-20">
+            <li>
+              <NavLink
+                to="/"
+                onClick={() => handleNavigation("/")}
+                className={({ isActive }) =>
+                  `${listItemStyles} ${isActive ? activeLinkStyles : ""} ${isHomePage ? (isScrolled ? 'after:bg-navyBlue' : 'after:bg-white') : 'after:bg-navyBlue'}`
+                }
+                id="gsapNavItem"
               >
-                <path
-                  fill="#071C40"
-                  d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
-                />
-              </svg>
-            </button>
+                <p>Home</p>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/portfolio"
+                onClick={() => handleNavigation("/portfolio")}
+                className={({ isActive }) =>
+                  `${listItemStyles} ${isActive ? activeLinkStyles : ""} ${isHomePage ? (isScrolled ? 'after:bg-navyBlue' : 'after:bg-white') : 'after:bg-navyBlue'}`
+                }
+                id="gsapNavItem"
+              >
+                <p>Our Work</p>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/services"
+                onClick={() => handleNavigation("/services")}
+                className={({ isActive }) =>
+                  `${listItemStyles} ${isActive ? activeLinkStyles : ""} ${isHomePage ? (isScrolled ? 'after:bg-navyBlue' : 'after:bg-white') : 'after:bg-navyBlue'}`
+                }
+                id="gsapNavItem"
+              >
+                <p>Services</p>
+              </NavLink>
+            </li>
+          </ul>
+          <div id="gsapLogo" className="static md2:absolute md2:left-1/2 md2:transform md2:-translate-x-1/2">
+            {isHomePage ? (isScrolled ? <Logo /> : <WhiteLogo />) : <Logo />} {/* Toggle logos based on page */}
           </div>
+
+          <NavLink to="#footerSection" id="gsapContactBtn" className="hidden md2:flex xl:w-[180px]">
+            <CustomButton2 text="Contact" />
+          </NavLink>
+
+          <button
+            type="button"
+            className="inline-flex items-center md2:hidden"
+            onClick={toggle}
+          >
+            {/* Menu icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              color={isHomePage ? (isScrolled ? "#071C40" : "#FFFFFF") : "#071C40"}
+            >
+              <path
+                fill={isHomePage ? (isScrolled ? "#071C40" : "#FFFFFF") : "#071C40"}
+                d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
+              />
+            </svg>
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

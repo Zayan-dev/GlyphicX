6b2bb854c7907f1c8d3172dfc9bb89cc3cd486d4
Zayan-dev <fa21bscs0095@maju.edu.pk>
2024-08-30 19:47:14 +0500

    services images resize

--- src/components/portfolio/project/Projects.jsx
+++ src/components/portfolio/project/Projects.jsx
@@ -364,7 +364,7 @@ const Projects = () => {
                     <h1 id='gsapleft' className='mainHeading' onClick={(e) => handleDisplay('project2', e)}>GlyphicX</h1>
                     <p id='gsapleft' className='para'>When it came to crafting our own identity, we approached the task with the same passion and creativity that we bring to every client project</p>
                     <p id='gsapleft' className='para mt-3 pb-3'>The result? A cohesive, powerful brand identity that not only represents who we are today but also inspires us as we continue to grow and evolve</p>
-                    <div className="mt-3">
+                    <div id='gsapleft' className="mt-3">
                         <button className='w-full' onClick={(e) => handleDisplay('project2', e)} >
                             <CustomButton2 text="Explore" ></CustomButton2>
                         </button>
@@ -380,7 +380,7 @@ const Projects = () => {
                     <h1 id='pro2gsapleft' className='mainHeading' onClick={(e) => handleDisplay('project1', e)}>Eaxee Enterprise</h1>
                     <p id='pro2gsapleft' className='para'>Eaxee, a leader in enterprise architecture management, sought a brand identity that would mirror its commitment to empowering organizations with intuitive, flexible solutions.</p>
                     <p id='pro2gsapleft' className='mt-3 para pb-3'>Our task was to craft a brand that embodies Eaxee’s mission of delivering a user-friendly platform while aligning with its vision of making enterprise architecture accessible to all.</p>
-                    <div className="mt-3">
+                    <div id='pro2gsapleft' className="mt-3">
                         <button className='w-full' onClick={(e) => handleDisplay('project1', e)} >
                             <CustomButton2 text="Explore" ></CustomButton2>
                         </button>
@@ -397,7 +397,7 @@ const Projects = () => {
                     <p id='pro3gsapleft' className='para'>Lambda Theta, a cutting-edge technology company, required a brand identity that would capture the essence of their expertise in providing research and development of AI-based and Digital Systems</p>
                     <p id='pro3gsapleft' className='mt-3 para pb-3'>We crafted a voice that resonates with Lambda Theta’s core mission—to solve complex challenges with customized, cutting-edge solutions establishing them as a trusted, innovative partner in the tech industry.</p>
 
-                    <div className="mt-3">
+                    <div id='pro3gsapleft' className="mt-3">
                         <button className='w-full' onClick={(e) => handleDisplay('project3', e)} >
                             <CustomButton2 text="Explore" ></CustomButton2>
                         </button>
@@ -413,7 +413,7 @@ const Projects = () => {
                     <h1 id='pro4gsapleft' className='mainHeading' onClick={(e) => handleDisplay('project4', e)}>uHu TvC Production</h1>
                     <p id='pro4gsapleft' className='para'>The ad highlights the universal application and strength of a UHU adhesive tube and was designed to leverage the hype around the release of House of the Dragon by nostalgia baiting a popular scene from Game of Thrones.</p>
                     <p id='pro4gsapleft' className='mt-3 para pb-3'>This pitch documents is the first step in the pre-production and essential to outline the perspective, vision, and story ideas for the job and explain how the TV commercial will be brought to life</p>
-                    <div className="mt-3">
+                    <div id='pro4gsapleft' className="mt-3">
                         <button className='w-full' onClick={(e) => handleDisplay('project4', e)} >
                             <CustomButton2 text="Explore" ></CustomButton2>
                         </button>

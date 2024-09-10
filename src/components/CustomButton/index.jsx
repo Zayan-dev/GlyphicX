7f7945a77a09a9b9f09c174ec742035b9425f154
Zayan-dev <fa21bscs0095@maju.edu.pk>
2024-09-10 20:06:02 +0500

    custombtn5 added

--- src/components/CustomButton/index.jsx
+++ src/components/CustomButton/index.jsx
@@ -32,7 +32,7 @@ export const CustomButton4 = ({text, onClick}) => {
   );
 }
 
-const CustomButton5 = ({text, onClick, type="button"}) => {
+export const CustomButton5 = ({text, onClick, type="button"}) => {
   return (
     <button type={type} className="relative h-11 sm:h-[50px] w-full min-w-28 sm:min-w-32 items-center justify-center rounded overflow-hidden bg-navyBlue font-medium text-offWhite shadow-md transition-all duration-300 before:absolute before:inset-0 before:border-0 before:border-offWhite before:duration-100 before:ease-linear hover:bg-offWhite hover:text-navyBlue hover:border-navyBlue hover:border-[1px] hover:before:border-[25px]">
       <span className="relative subHeading text-base sm:text-lg lg:text-xl">

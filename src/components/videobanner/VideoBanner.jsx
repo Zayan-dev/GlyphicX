9a98f921b6477e828504b89b70d89188562036e8
Fatima Zehra <fz8585949@gmail.com>
2024-11-11 17:52:18 +0500

    Video controls

--- src/components/videobanner/VideoBanner.jsx
+++ src/components/videobanner/VideoBanner.jsx
@@ -1,35 +1,19 @@
 import React, { useState, useRef } from "react";
 import "../videobanner/videoBanner.css";
 import banner from "../../assets/videos/banner_video.mp4";
-import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 
 const VideoBanner = () => {
-  const [isMuted, setIsMuted] = useState(true); // Track mute state
-  const videoRef = useRef(null);
-
-  // Toggle mute/unmute and update icon
-  const handleMuteToggle = () => {
-    if (videoRef.current) {
-      videoRef.current.muted = !isMuted;
-      setIsMuted(!isMuted); // Toggle mute state
-    }
-  };
-
   return (
     <div className="video-banner">
       <video
         className="hero-video"
-        ref={videoRef}
         src={banner}
         muted // Initially muted for autoplay
         autoPlay
         playsInline
         loop
+        controls
       />
-      {/* Mute/Unmute Icon */}
-      <button onClick={handleMuteToggle} className="sound-icon-btn">
-        {isMuted ? <HiSpeakerXMark size={24} /> : <HiSpeakerWave size={24} />}
-      </button>
     </div>
   );
 };
